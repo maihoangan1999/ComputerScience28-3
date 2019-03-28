fe40d7342def83a36bde53a16cab52e8776d0b7e
phongdang707 <phongdang707@gmail.com>
2019-03-28 15:32:04 +0700

    an

---
+++ WebApplication1/WebApplication1/Controllers/ArrayListController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ArrayListController : Controller
+    {
+        static private object[] Buffer = new object[10];
+        static private int Length = 0;
+        public ActionResult Index()
+        {
+            var model = new Tuple<object[], int>(Buffer, Length);
+            return View(model);
+        }
+        public ActionResult Append(string value)
+        {
+            Buffer[Length] = value;
+            Length++;
+            return RedirectToAction("Index");
+        }
+        public ActionResult Clear()
+        {
+            Length = 0;
+            return RedirectToAction("Index");
+        }
+        public ActionResult InsertAt(int index, string value)
+        {
+            for (int i = Length - 1; i >= index; i--)
+            
+                Buffer[i + 1] = Buffer[i];
+                Buffer[index] = value;
+                Length++;
+       
+            
+            return RedirectToAction("Index");
+        }
+	}
+}
